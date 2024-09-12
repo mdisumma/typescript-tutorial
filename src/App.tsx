5f748f697cccad0ab01f80d8d659ca0bde939d87
Marco Di Summa <mdisumma@yahoo.com>
2024-09-12 14:27:19 +0100

    setting up project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from "./components/navigation/Navigation";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Typescript tutorial</h1>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+export default function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>Compatibility with JavaScript</li>
+        <li>Static Typing</li>
+        <li>Type Inference</li>
+        <li>Optional and Default Parameters</li>
+        <li>Interfaces</li>
+        <li>Enum Types</li>
+        <li>Generics</li>
+        <li>Modules and Namespaces</li>
+        <li>Advanced Object-Oriented Features</li>
+        <li>Enhanced IDE Support</li>
+      </ul>
+    </nav>
+  );
+}
